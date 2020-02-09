e4d2239342f8de42a4eb66b9479ed70688a2e361
Nduka-Eze Ariguzo <u.ariguzo.ua@gmail.com>
2020-02-09 01:26:15 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,25 +1,52 @@
+array_1 = [
+  ['pepperoni', 'sausage'], 
+  ['green olives', 'green peppers'], 
+  ['onions', 'pineapple']
+]
+
 def join_ingredients(src)
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
+  new_array = ["I love #{src[0][0]} and #{src[0][1]} on my pizza", "I love #{src[1][0]} and #{src[1][1]} on my pizza", "I love #{src[2][0]} and #{src[2][1]} on my pizza"]
+new_array
 end
+
+
+array_2 = [[-1, -900], [10, 30], [0, 0], [14, 16 * -2.5], [Math.sin(1), 19]]
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+  inner_array = []
+  row_index = 0
+  while row_index < src.count do
+    element_index = 1
+    while element_index < src[row_index].count do
+      inner_array << src[row_index].max
+      element_index += 1
+    end
+    row_index += 1
+  end
+inner_array
 end
 
+array = [
+  [86262, 58115], 
+  [22558, 97709], 
+  [66236, 28056], 
+  [25992, 81375], 
+  [11248, 56789], 
+  [42783, 27353], 
+  [70796, 63432], 
+  [53234, 63058], 
+  [72316, 8675309]
+]
+
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+  row_index = 0
+  total = 0
+  while row_index < src.length do
+    if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0
+      total += src[row_index][0]
+      total += src[row_index][1]
+    end
+    row_index += 1
+  end
+  total
 end
